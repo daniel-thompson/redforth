22c42b53c4d24bf90f20132c4c1fc2d654b80c87
Daniel Thompson <daniel@redfelineninja.org.uk>
2021-11-26 19:54:36 +0000

    script/encode.fs: Add a direct mode.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- scripts/encode.fs
+++ scripts/encode.fs
@@ -1,5 +1,29 @@
+#!/usr/bin/env redforth
 \ SPDX-License-Identifier: LGPL-3.0-or-later
 
+(
+  Encode an ASCII file as Forth source.
+
+  This allows us to copy files to Forth target devices such as the
+  Raspberry Pi Pico.
+
+  It has two operating modes. Firstly, it can be used directly to encode
+  files. For example:
+
+      picocom --send-cmd scripts/encode.fs /dev/ttyACM0
+
+  Secondly, we can encode into a temporary Forth file to generates the
+  expected source. This is useful for testing the encoder and to handle
+  more exotic cases. For example:
+
+      redforth include scripts/encode.fs \
+               encode myscript.fs bye \
+               > myscript.xfr
+      picocom --send-cmd "ascii-xfr -sd" /dev/ttyACM0
+
+  From that point we can transfer the file to a Rasperry Pi Pico by
+  issuing MOUNT to Forth and C-x C-s to picocom.
+)
 
 : FILEID:CAT	( fileid -- )
 	BEGIN
@@ -47,9 +71,7 @@
 	-ROT 2DROP
 	;
 
-: ENCODE
-	WORD
-
+: ENCODE-FILE
 	( header )
 	S\" S\" " TYPE
 	2DUP TYPE
@@ -81,4 +103,16 @@
 
 	( footer )
 	S\" CLOSE-FILE DROP\n" TYPE
+	;
+
+: ENCODE
+	WORD ENCODE-FILE
+	;
+
+: MAIN
+	ARGC @ IF
+		ARGC @ 1 DO
+			I ARG ENCODE-FILE
+		LOOP
+	THEN
 	;
