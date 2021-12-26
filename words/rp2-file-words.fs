b9ebb2a82849491c4b2cde2c79f0e93c5a2be37b
Daniel Thompson <daniel@redfelineninja.org.uk>
2021-12-26 14:59:15 +0000

    READ-LINE: Enable for RP2 Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- words/rp2-file-words.fs
+++ words/rp2-file-words.fs
@@ -67,3 +67,63 @@
 		SOURCE-ID !
 	THEN
 	;
+
+: READ-LINE-KEY	( fileid c-addr u1 u2 -- fileid c-addr u1 u2 ior)
+	ROT		( fileid u1 u2 c-addr )
+	DUP 1 5 PICK	( fileid u1 u2 c-addr c-addr 1 fileid )
+	READ-FILE	( fileid u1 u2 c-addr 1 ior )
+	DUP IF
+		SWAP DROP	( fileid u1 u2 c-addr ior )
+		>R -ROT R>	( fileid c-addr u1 u2 ior )
+	ELSE
+		DROP 0 = IF	( fileid u1 u2 c-addr )
+			-ROT 0
+			EXIT
+		THEN
+		1+ -ROT 1+	( fileid c-addr+1 u1 u2+1 )
+		1		( fileid c-addr u1 u2 ior )
+	THEN
+	;
+
+: READ-LINE-LF? ( c-addr u1 u2 -- c-addr u1 u2 bool )
+	2 PICK 1- C@ 10 =
+	;
+
+: READ-LINE-EXIT ( fileid c-addr u1 u2 -- u2 TRUE 0 )
+	-ROT 2DROP
+	SWAP DROP
+	TRUE 0
+	;
+
+: READ-LINE	( c-addr u1 fileid -- u2 flag ior )
+	-ROT 0		( fileid c-addr u1 0 )
+	BEGIN
+		READ-LINE-KEY DUP 1 =
+	WHILE		( fileid c-addr u1 u2 ior )
+		DROP		( fileid c-addr u1 u2 )
+		READ-LINE-LF?
+		IF
+			READ-LINE-EXIT
+			EXIT
+		THEN
+
+		2DUP < IF
+			READ-LINE-EXIT
+			EXIT
+		THEN
+	REPEAT		( fileid c-addr u1 u2 ior )
+	( READ-LINE-KEY TOS is 0 at EOF, 1 otherwise )
+	DUP 0 > IF
+		DROP
+		0
+	THEN
+	>R
+	-ROT 2DROP	( fileid u2 )
+	SWAP DROP	( u2 )
+	R>		( u2 ior )
+	DUP 0= SWAP	( u2 flag ior )
+	;
+
+HIDE READ-LINE-KEY
+HIDE READ-LINE-LF?
+HIDE READ-LINE-EXIT
